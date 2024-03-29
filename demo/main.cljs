05939e2bde86c6d9fb4243f7c56d2cfcd245014c
Martin Klepsch <martinklepsch@googlemail.com>
2024-03-29 13:23:20 +0200

    make HMR work (index.js redirect) (#3)

--- demo/main.cljs
+++ demo/main.cljs
@@ -1,1 +1,5 @@
-(js/console.log "test")
+(defn main []
+  (let [h1 (js/document.createElement "h1")]
+    (set! (.-textContent h1) "xxx")
+    (js/document.body.appendChild h1))
+  (js/console.log "fooba"))
--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
+import Inspect from 'vite-plugin-inspect'
 // import squint from 'vite-plugin-squint'; // FIXME somehow this isn't updated when updating the source file and running yarn install
 import squint from '../index.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    squint
+    squint,
+    Inspect()
   ]
 });
