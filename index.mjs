3297d9914895ccb26ba67ff12dcd41b070c71e39
Brandon Stubbs <brandon@bos-solutions.co.uk>
2024-03-30 00:24:19 -0400

    hmr

--- index.mjs
+++ index.mjs
@@ -1,21 +1,45 @@
 import { compileString } from "squint-cljs";
-import esbuild from "esbuild";
+import path, { dirname } from "path";
+import fs from "fs";
 
-// TODO: squint source mapping
 export default function viteSquint(opts = {}) {
   const squint = {
     name: "squint_compile",
-    transform: function (code, id) {
+    enforce: "pre",
+    async load(id) {
+      // the resolveId adds the .jsx extension
+      if (/\.cljs.jsx$/.test(id)) {
+        // for cljs files, we just need to load and compile
+        // TODO: macros
+        // TODO: squint source mapping
+        const file = id.replace(/.jsx$/, "");
+        const code = await fs.promises.readFile(file, "utf-8");
+        const compiled = compileString(code);
+        return { code: compiled, map: null };
+      }
+    },
+    resolveId(id, imported) {
       if (/\.cljs$/.test(id)) {
-        const cs = compileString(code);
-        const jsx = esbuild.transformSync(cs, {
-          loader: "jsx",
-          jsx: "automatic",
-        });
-        return {
-          code: jsx.code,
-          map: null,
-        };
+        // For cljs files we need to do the following:
+        // absolutize the path, this makes it easier for load and other plugins
+        // append .jsx so that other plugins can pick it up
+        const absolutePath = path.resolve(dirname(imported), id);
+        return absolutePath + ".jsx";
+      }
+    },
+    handleHotUpdate({file, server, modules }) {
+      if (/\.cljs$/.test(file)) {
+        // this needs to be the same id returned by resolveId this is what
+        // vite uses as the modules identifier
+        const resolveId = file + ".jsx";
+        const module = server.moduleGraph.getModuleById(resolveId);
+        if (module) {
+          // invalidate dependants
+          server.moduleGraph.onFileChange(resolveId);
+          // hot reload
+          return [...modules, module ]
+        }
+        return modules;
       }
     },
   };
